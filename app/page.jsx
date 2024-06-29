9d1c2b291467df8b2e27c4b66bf8d0c6450696a3
jadenyoky <jaden_yoky@yahoo.com>
2024-06-29 22:22:44 +0300

    test only ...

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,37 @@
+"use client";
+import axios from "axios";
+import { useRef, useState } from "react";
+
 export default function Home() {
+  const verse = useRef();
+  const [data, setdata] = useState([]);
+  const [loading, setloading] = useState(false);
+
+  const url = "https://api.quran.com/api/v4/quran/verses/code_v2?page_number=4";
+  const apiVerse = async function () {
+    setloading(false);
+
+    const apiA = await axios.get(url);
+    setdata(apiA.data.verses);
+    console.log(apiA);
+    setloading(true);
+  };
+
   return (
     <>
       <div>QURANIUM</div>
+      <button onClick={() => apiVerse()}>get</button>
+      {loading ? (
+        data.map((e, k) => {
+          return (
+            <h2 ref={verse} key={k}>
+              {e.code_v2}
+            </h2>
+          );
+        })
+      ) : (
+        <h2>loading</h2>
+      )}
     </>
   );
 }
