a5d596edd015e8d19966130aebf0951fa71ec859
jadenyoky <jaden_yoky@yahoo.com>
2024-08-02 18:41:20 +0300

    test svh with remove body 100vh

--- components/theme/ThemeRegistry.jsx
+++ components/theme/ThemeRegistry.jsx
@@ -4,6 +4,7 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useEffect, useState } from "react";
 import DarkMode from "./darkMode";
 import store from "store2";
+import { Skeleton, Typography } from "@mui/material";
 
 export default function ThemeRegistry({ children }) {
   const stored = store("theme");
@@ -23,16 +24,18 @@ export default function ThemeRegistry({ children }) {
 
   if (!loading) {
     return (
-      <div
-        className="h-lvh"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: "5px",
-        }}
-      >
-        <h1>loading...</h1>
+      <div className="loading">
+        <Skeleton animation="wave" variant="circular" width={70} height={70} />
+        <Skeleton variant="rectangular" width={"80%"} height={"15%"} />
+        <Typography variant="inherit" color="white">
+          Loading ...
+        </Typography>
+        <Skeleton
+          animation="wave"
+          variant="rounded"
+          width={"80%"}
+          height={"30%"}
+        />
       </div>
     );
   }
